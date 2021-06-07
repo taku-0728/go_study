61202fd40b1991c65ecfd99aac9ce7adec1a4427
taku-0728 <taku8270@gmail.com>
2021-06-07 23:17:24 +0900

    [add] toascii.go

---
+++ simpleget/toascii.go
@@ -0,0 +1,16 @@
+// 例3-20
+package main
+
+import (
+    "fmt"
+    "golang.org/x/net/idna"
+)
+
+func main() {
+    src := "握力王"
+    ascii, err := idna.ToASCII(src)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("%s -> %s\n", src, ascii)
+}
